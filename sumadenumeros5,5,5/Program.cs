5f7251766bfb921b0bfa559401a8ffc02fe9283e
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 21:08:52 -0400

    Agregar archivos de proyecto.

---
+++ sumadenumeros5,5,5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sumadenumeros5_5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(sumaNum(444488888848));
+        }
+        public static long sumaNum(long num)
+        {
+            string u;
+            long a = 0;
+            long res = 0;
+            string numString = Convert.ToString(num);
+            for (int i=0;i<=numString.Length-1;i++)
+            {
+                u = Convert.ToString(numString[i]);
+                a = Convert.ToInt32(u);
+                res = res + a;
+
+            }
+            return res;
+        }
+    }
+}
